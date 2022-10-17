5327ce3367fdaae9f994e781ed69c720efa1f599
Samreensaba <Samren_saba@yahoo.com>
2022-10-17 12:48:19 +0100

    update

--- project/src/components/MyNav.jsx
+++ project/src/components/MyNav.jsx
@@ -1,37 +1,36 @@
 import { Link } from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const MyNav = () => {
   return (
-    <nav className="navbar-expand-md navBar">
-      <Link className="navbar-brand" style={{ color: "white" }} to="/">West 3 Solutions</Link>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/" style={{ color: "white" }}>Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" style={{ color: "white" }} to="/registration">Register CV</Link>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" style={{ color: "white" }} href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-              Services
-            </a>
-            <div className="dropdown-menu">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="#">Something else here</a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" style={{ color: "white" }}>About</a>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/ContactUs" style={{ color: "white" }}>Contact Us</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  )
+    <Navbar bg="light" expand="lg" className="navBar">
+      <Container>
+        <Link className="navbar-brand" style={{ color: "white" }} to="/">West 3 Solutions</Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/" style={{ color: "white" }}><Nav.Link style={{color:"white"}}>Home</Nav.Link></Link>
+            <Link to="/registration"><Nav.Link style={{color:"white"}}>Register CV</Nav.Link></Link>
+            {/* <NavDropdown style={{textDecoration:"none", color:"white"}} title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+            <Link to="/ContactUs"><Nav.Link style={{ color: "white" }}>Contact US</Nav.Link></Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+ 
 export default MyNav
